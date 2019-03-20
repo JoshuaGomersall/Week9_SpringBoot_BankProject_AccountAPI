87d6fdd50e39847b52bd9270303ec922f2011ce1
Jordan <ecrofiar@gmail.com>
2019-03-20 05:32:53 -0400

    added Rest templates into the Account Controller class

--- src/main/java/com/example/restcontroller/AccountController.java
+++ src/main/java/com/example/restcontroller/AccountController.java
@@ -1,7 +1,19 @@
 package com.example.restcontroller;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.entities.AccountEntity;
 import com.example.service.AccountService;
@@ -21,7 +33,28 @@ public class AccountController {
 		this.accService.createAccount(account);
 		
 	}
+	  @Autowired
+	   RestTemplateBuilder restTemplate;
+
 	
+	  @RequestMapping(value = "template/createAccount", method = RequestMethod.POST)
+	   public String AccountApi(@RequestBody AccountEntity account) {
+	      HttpEntity<AccountEntity> entity = new HttpEntity<AccountEntity>(account,null);
+	      
+	      return restTemplate.build().exchange(
+	         "http://localhost:8883/", HttpMethod.POST, entity , String.class).getBody();
+	   
+	      
+	  }
+	 
+	  @RequestMapping(value = "template/createAccount", method = RequestMethod.POST)
+	   public String createProducts(@RequestBody AccountEntity account) {
+	      HttpEntity<AccountEntity> entity = new HttpEntity<AccountEntity>(account,null);
+	      
+	      return restTemplate.build().exchange(
+	         "http://localhost:8882/", HttpMethod.GET, entity , String.class).getBody();
+	   
+	  }
 	
 
 }
